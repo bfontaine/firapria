214f13742c296ad81be7921ed76842d0bbab320f
Baptiste Fontaine <batifon@yahoo.fr>
2014-03-22 11:39:47 +0100

    minor refactoring

--- firapria/cli.py
+++ firapria/cli.py
@@ -18,44 +18,41 @@ def print_version_and_exit():
     sys.exit(0)
 
 
-def colorize_indices(indices, max_value):
-    def colorize(i):
-        c = Fore.RESET
+def colorize_indice(i, max_value):
+    c = Fore.RESET
 
-        if i >= max_value:
-            c = Fore.YELLOW
-        elif i > 0.75 * max_value:
-            c = Fore.RED
-        elif i > 0.5 * max_value:
-            c = Fore.MAGENTA
-        elif i > 0.25 * max_value:
-            c = Fore.GREEN
-        else:
-            c = Fore.WHITE
+    if i >= max_value:
+        c = Fore.YELLOW
+    elif i > 0.75 * max_value:
+        c = Fore.RED
+    elif i > 0.5 * max_value:
+        c = Fore.MAGENTA
+    elif i > 0.25 * max_value:
+        c = Fore.GREEN
+    else:
+        c = Fore.WHITE
 
-        return "%s%d%s" % (c, i, Fore.RESET)
-
-    return map(colorize, indices)
+    return "%s%d%s" % (c, i, Fore.RESET)
 
 
 def print_pollution():
     p = PollutionFetcher()
-    print """Pollution:
-    Yesterday: %s
-    Today: %s
-    Tomorrow: %s""" % tuple(colorize_indices(p.indices(), max_value=100))
+    days = ['Yesterday', 'Today', 'Tomorrow']
+
+    indices = zip(days, p.indices())
+
+    print "Pollution:"
+    for day, indice in indices:
+        print "\t%s: %s" % (day, colorize_indice(indice, 100))
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Get pollution indices for Paris'
     )
-    parser.add_argument('--pollution', '-p', dest='pollution',
-                        default=True, help='print pollution info')
     parser.add_argument('--version', '-v', action='store_true')
     args = parser.parse_args()
     if args.version:
-        print_version_and_exit()
+        return print_version_and_exit()
 
-    if args.pollution:
-        print_pollution()
+    print_pollution()
